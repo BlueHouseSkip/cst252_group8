e66c78ad8b7393cc5f5878e90f800a6938cc09a3
BlueHouseSkip <bluehouseskip@gmail.com>
2022-04-13 11:39:19 -0700

    testing modified
    testing is cleaner and now is set up for 10 questions

--- js/testing.js
+++ js/testing.js
@@ -1,7 +1,7 @@
 
 
 // weight is how much to count each question as. decimal percent so 1 is 100%, .5 is 50%, 2 is 200%, etc.
-const WEIGHT = [1,1,1,1.5,1];
+const WEIGHT = [1,1,1,1,1,1,1,1,1,1];
 
 // 5 questions so far
 // each question has 4 answers, represneted by 1 2 3 and 4 in each one
@@ -22,17 +22,25 @@ function Cartoon (name, answersArray, element) {
       });
     return similarityNum;
   }
+  this.enableElement = function () {
+    // enable the element showing the results for their quiz
+    return;
+  }
 }
 
+/*
+    Cartoon cosntructor is:
+        name of cartoon, list of correct answers, DOM object to enable
+*/
 
-const GRAVITYFALLS    = new Cartoon("Gravity Falls",              [1,4,2,3,2], "non");
-const RICKNMORTY      = new Cartoon("Rick and Morty",             [2,1,3,1,3], "non");
-const STARWARS        = new Cartoon("Star Wars: the Clone Wars",  [4,2,3,1,4], "non");
-const SAILORMOON      = new Cartoon("Sailor Moon",                [1,3,2,4,1], "non");
-const STEVEN          = new Cartoon("Steven Universe",            [3,4,4,2,1], "non");
-const ARCANE          = new Cartoon("Arcane",                     [1,1,1,1,1], "non");
+const GRAVITYFALLS    = new Cartoon("Gravity Falls",              [1,4,2,3,2,3,1,2,4,3], "non");
+const RICKNMORTY      = new Cartoon("Rick and Morty",             [2,1,3,1,3,4,4,2,2,2], "non");
+const STARWARS        = new Cartoon("Star Wars: the Clone Wars",  [4,2,3,1,4,3,1,1,3,2], "non");
+const SAILORMOON      = new Cartoon("Sailor Moon",                [1,3,2,4,1,3,2,3,4,1], "non");
+const STEVEN          = new Cartoon("Steven Universe",            [3,4,4,2,1,1,3,2,2,3], "non");
+const ARCANE          = new Cartoon("Arcane",                     [1,1,1,1,1,4,3,4,4,2], "non");
 
-const cartoonArray = [GRAVITYFALLS, RICKNMORTY, STARWARS, SAILORMOON, STEVEN, ARCANE];
+const CARTOON_ARRAY = [GRAVITYFALLS, RICKNMORTY, STARWARS, SAILORMOON, STEVEN, ARCANE];
 // $("#14")[0].checked
 /*
 $("#sum").click(function() {
@@ -42,27 +50,33 @@ $("#sum").click(function() {
 
 $("#sum").click(function() {
 //  console.log("clicked");
+  if($(":checked").length != WEIGHT.length) {
+    alert("Please answer every question");
+    return;
+  }
   var inputAnswers = [];
   $(":checked").each(function (i) {
       inputAnswers[i] = parseInt(this.value);
+//      inputAnswers[i] += Math.random() * 0.3;
 //      console.log(this);
   });
- console.log(inputAnswers);
-  var similar = gravityFalls.similarity(inputAnswers);
-// console.log(similar);
- similarityArray = [];
-  cartoonArray.forEach(function (e, i, r) {
-    similarityArray[i] = (cartoonArray[i].similarity(inputAnswers));
+  console.log("Your answers:");
+  console.log(inputAnswers);
+  similarityArray = [];
+  CARTOON_ARRAY.forEach(function (e, i, r) {
+    similarityArray[i] = (CARTOON_ARRAY[i].similarity(inputAnswers));
 
   });
+  console.log("similarity:");
+  console.log(similarityArray);
   let max = Math.max(...similarityArray);
 
   let index = similarityArray.indexOf(max);
-//  console.log(index);
-  console.log(cartoonArray[index].name);
+  console.log(index);
+  console.log(CARTOON_ARRAY[index].name);
+  CARTOON_ARRAY[index].enableElement();
+
 });
-
-
 
 
 // more space
