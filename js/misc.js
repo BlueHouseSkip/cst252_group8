47f42fa416a09ec5a219543f3923a1c318e1e199
BlueHouseSkip <bluehouseskip@gmail.com>
2022-05-11 10:56:21 -0700

    rainbpw nav

--- js/misc.js
+++ js/misc.js
@@ -2,3 +2,11 @@
 $("#scroll_button").click(function () {
   window.scrollTo(0,0);
 });
+
+
+setInterval(rainbows, 10);
+
+function rainbows () {
+  let newColor = $("#description").css("background-color");
+  $(".bg-custom").css("background-color", newColor);
+}
